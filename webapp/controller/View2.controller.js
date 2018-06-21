1ce65650a5e034e2b206876c05ba31f86caa10ca
mradivojevic <milos.radivojevic.ns@gmail.com>
2018-06-21 13:35:16 +0000

    radi model i za combobox

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -12,9 +12,13 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf mr.sapui.SUI5-1.view.View2
 		 */
-			onInit: function() {
-				this._router = sap.ui.core.UIComponent.getRouterFor(this);
-			},
+		onInit: function() {
+			
+			this._router = sap.ui.core.UIComponent.getRouterFor(this);
+			this._model = this.getOwnerComponent().getModel("MojModel");
+			this.getView().setModel(this._model);
+
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -41,14 +45,17 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
-		nazad: function(){
-				//history.go(-1);
-				this._router.navTo("RouteView1");
+		nazad: function() {
+			//history.go(-1);
+			this._router.navTo("RouteView1");
 		},
-		popup: function(){
-			var model = this.getOwnerComponent().getModel("MojModel");
-			var objm = model.getObject("/MM");
+		popup: function() {
+			var objm = this._model.getObject("/MM");
 			MessageBox.success(objm.zaglavlje.pod1);
+			
+			var combolist = this.getView().byId("box0");
+			MessageBox.success(combolist.getSelectedKey());
+			
 		}
 	});
 
