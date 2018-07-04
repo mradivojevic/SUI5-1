495db2c6c7b9ffe90b5959e46253707db2a4eaa6
mradivojevic <milos.radivojevic.ns@gmail.com>
2018-07-04 09:50:30 +0000

    Konacno uspeo da mi radi oData pozivanje funkcije

--- webapp/controller/View3.controller.js
+++ webapp/controller/View3.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function(Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("mr.sapui.SUI5-1.controller.View3", {
@@ -10,45 +11,47 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf mr.sapui.SUI5-1.view.View3
 		 */
-			onInit: function() {
+		onInit: function() {
 			this._router = sap.ui.core.UIComponent.getRouterFor(this);
-			},
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf mr.sapui.SUI5-1.view.View3
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf mr.sapui.SUI5-1.view.View3
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+			var url = "/sap/opu/odata/sap/ZRM_PROBA_SRV";
+			this._model = new sap.ui.model.odata.v2.ODataModel(url);
+		},
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 		 * @memberOf mr.sapui.SUI5-1.view.View3
 		 */
-		//	onExit: function() {
+		//  onExit: function() {
 		//
-		//	}
-	akc1: function(){
-		// registruj odata servis.
-		// http://sapides.mi-system.co.rs:8000/sap/opu/odata/sap/ZRM_PROBA_SRV/ZbirFun?iPrvi=100&iDrugi=10
-		var url =  "/sap/opu/odata/sap/ZRM_PROBA_SRV/";
-		var oModel = new sap.ui.model.odata.v2.ODataModel( url );
-		// pozovi funkciju sa odata servisa.
-	},
-	nazad: function() {
+		//  }
+
+		akc1: function() {
+			// /sap/opu/odata/sap/ZRM_PROBA_SRV/ZbirFun?iPrvi=100&iDrugi=1
+			var prvi = this.getView().byId("input1").getValue();
+			var drugi = this.getView().byId("input2").getValue();
+			
+			this._model.callFunction("/ZbirFun", {
+				method: "POST",
+				urlParameters: {
+					"iPrvi": prvi,
+					"iDrugi": drugi
+				},
+				success: this.pozUsp,
+				error: this.pozGresk
+			});
+
+		},
+		nazad: function() {
 			//history.go(-1);
 			this._router.navTo("RouteView1");
+		},
+
+		pozUsp: function(odata, response) {
+			MessageBox.success("Rezultat: " + odata.Zbir);
+		},
+
+		pozGresk: function(oerror) {
+			MessageBox.alert(oerror);
 		}
 	});
 
